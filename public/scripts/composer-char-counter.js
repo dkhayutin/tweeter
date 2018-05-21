dbe924f28ed318e6482f7f7dcbf3113af4162de9
dkhayutin <dan.khayutin@gmail.com>
2018-05-21 20:05:56 +0000

    character counter implemented

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,11 @@
-$(document).ready(function (){
-  $('.new-tweet textarea').on('input', function(){
-
-   var character = $(this).val().length
-   $(this).parent().find('.counter').text(140 - character)
-    if( 140 - character < 0) {
-      $(".counter").addClass('redText')
-    } else {
-      $(".counter").removeClass('redText')
-    }
-  });
+$(document).ready(function() {
+    $('.new-tweet textarea').on('input', function(){
+      var character = $(this).val().length
+      $(this).parent().find('.counter').text(140 - character)
+      if(140 - character < 0) {
+        $('.counter').addClass('redText')
+      } else {
+        $('.counter').removeClass('redText')
+      }
+    })
 });
